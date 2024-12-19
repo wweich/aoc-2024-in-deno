91a0626b168fbf28370892ba0758efe3ff4f9428
Wulf Weich <wulf@weich-kr.de>
2024-12-19 09:57:27 +0100

    day 19

---
+++ day-19/part2.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env deno
+
+const input = await Deno.readTextFile(
+    `${import.meta.dirname}/${
+        Deno.args.includes("test") ? "testinput.txt" : "input.txt"
+    }`,
+);
+
+const [towelsStr, designStr] = input.trim().split("\n\n");
+const towels = towelsStr.split(",").map((t) => t.trim());
+const designs = designStr.split("\n");
+
+let possibleDesignCount = 0;
+
+for (const design of designs) {
+    const check = new Array(design.length + 1).fill(0);
+    check[0] = 1;
+    for (let i = 0; i < design.length; i++) {
+        if (check[i]) {
+            for (const towel of towels) {
+                if (design.slice(i, i + towel.length) === towel) {
+                    check[i + towel.length] += check[i];
+                }
+            }
+        }
+    }
+    possibleDesignCount += check[design.length];
+}
+
+console.log(possibleDesignCount);
